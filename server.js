7e36fcc42bf13ced4ba82d00d42c4c7053880bac
mateomh <mmojicah.84@gmail.com>
2021-02-11 20:57:35 -0500

    Created the get root endpoint

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const app = express();
+const server_port = 3000;
 
-app.listen(5000, () => console.log("Server Started on port 5000"));
+app.get('/', (request, response) => {
+  console.log('Request', request);
+  response.send('<h1>Hello from node</h1>');
+});
 
+app.listen(server_port, () => console.log(`Server Started on port ${server_port}`));
