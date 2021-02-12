cf16ab6aa274b63e22bf17f7ca61f60cb6b5ca79
mateomh <mmojicah.84@gmail.com>
2021-02-12 10:45:23 -0500

    Add environment variables for the appid and key

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const Token = require('./modules/token_generator');
 
+require('dotenv').config();
+
 const secure_key = fs.readFileSync('./key.pem');
 const cert = fs.readFileSync('./cert.pem');
 
@@ -11,18 +13,17 @@ const app = express();
 const server = https.createServer({key: secure_key, cert: cert }, app);
 const server_port = 3000;
 
-const key = 'rUlaMASgt1Byi4Kp3sKYDeQzo';
-const appId = 'ApplicationID';
-const expires = 10000;
+
 
 app.get('/', (request, response) => {
   let userName = 'user1';
+  const expires = 10000;
 
   if (request.headers.user !== undefined){
     userName = request.headers.user;
   }
 
-  const token = Token.generateToken(key, appId, userName, expires);
+  const token = Token.generateToken(process.env.APP_KEY, process.env.APP_ID, userName, expires);
   response.send({ token });
 });
 
