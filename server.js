2cc14b8d8786e4df80d106338f903104803f7813
mateomh <mmojicah.84@gmail.com>
2021-02-12 20:46:05 -0500

    Add cors requests to be processed for the server

--- server.js
+++ server.js
@@ -10,13 +10,21 @@ const secure_key = fs.readFileSync('./key.pem');
 const cert = fs.readFileSync('./cert.pem');
 
 const app = express();
+
+// Allows CORS to be enabled
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, user, Content-Type, Accept");
+  next();
+});
+
 const server = https.createServer({key: secure_key, cert: cert }, app);
 const server_port = 3000;
 
-
-
 app.get('/', (request, response) => {
   let userName = 'user1';
+
+  console.log('User', request.headers.user)
   const expires = 10000;
 
   if (request.headers.user !== undefined){
